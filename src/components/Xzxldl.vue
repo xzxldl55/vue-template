00433f361f43274e75a6c53c1c8e23c704603ca3
xzxldl55 <476630095@qq.com>
2019-07-31 18:08:13 +0800

    change template

---
+++ src/components/Xzxldl.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Hello xzxldl</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'xzxldl'
+    }
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        width: 100%;
+        text-align: center;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Xzxldl from '../components/Xzxldl'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'xzxldl',
+      component: Xzxldl
     }
   ]
 })
